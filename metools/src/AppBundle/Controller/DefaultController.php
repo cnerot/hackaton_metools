a34582102ff40f79ac947d32ed392ccb2783f1ee
test <test@gmail.com>
2017-04-28 05:22:44 +0200

    back_login

--- metools/src/AppBundle/Controller/DefaultController.php
+++ metools/src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
@@ -14,6 +16,26 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
+        $request->request->all();
+        //var_dump($request);
+
+        if (isset($_POST['_login']) && isset($_POST['_password'])){
+            $login = $_POST['_login'];
+            $password = $_POST['_password'];
+
+            $user = $this->getDoctrine()
+                ->getRepository('AppBundle:user')
+                ->findBy(
+                    array('login' => $login,'password' => $password)
+
+                );
+
+            var_dump($user);
+
+
+        }
+
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
@@ -24,10 +46,18 @@ class DefaultController extends Controller
      */
     public function loginAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/login.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('default/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
     }
 
     /**
@@ -35,12 +65,46 @@ class DefaultController extends Controller
      */
     public function InscriptionAction(Request $request)
     {
+
+
         // replace this example code with whatever you need
         return $this->render('default/inscription.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+    /**
+     * @Route("/CreateUser", name="CreateUser")
+     */
+    public function CreateUserAction(Request $request)
+    {
+       $request->request->all();
 
+        if (isset($_REQUEST['_login']) && isset($_REQUEST['_password']) && isset($_REQUEST['_password_confirm']) && $_REQUEST['_password_confirm']==$_REQUEST['_password'])
+        {
+            $user = new user();
+            $user -> setLogin($_REQUEST['_login']);
+            $user -> setName(' ');
+            $user -> setSurname(' ');
+            $user -> setAge('5');
+            $user->setAddress('xxx   ');
+            $user -> setPassword($_REQUEST['_password']);
+            $em = $this->getDoctrine()->getManager();
+
+            // tells Doctrine you want to (eventually) save the Product (no queries yet)
+            $em->persist($user);
+
+            // actually executes the queries (i.e. the INSERT query)
+            $em->flush();
+            return $this->render('default/success.html.twig');
+        }
+        else{
+
+            return $this->render('default/inscription_erreur.html.twig');
+        }
+
+
+
+    }
 
     /**
      * @Route("/game", name="game")
@@ -73,5 +137,12 @@ class DefaultController extends Controller
         return $this->render('default/evolution.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+    /**
+     * @Route("/evolution", name="evolution")
+     */
+    function  getloggedAction(Request $request)
+    {
+        return $this->render('default/inscription.html.twig');
     }
 }
